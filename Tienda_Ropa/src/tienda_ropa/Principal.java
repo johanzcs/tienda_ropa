e40f2744ec87908a6a4b7c9a821cb2daa303887a
johanzcs <sebaszambranoc3@gmail.com>
2025-04-01 09:18:14 -0500

    Add files via upload

---
+++ Tienda_Ropa/src/tienda_ropa/Principal.java
@@ -0,0 +1,16 @@
+
+package tienda_ropa;
+
+import Vista.Registro_Producto;
+
+
+public class Principal {
+
+   
+    public static void main(String[] args) {
+       Registro_Producto obj_registros = new Registro_Producto ();
+       obj_registros.setVisible(true);
+    
+    
+}
+}
